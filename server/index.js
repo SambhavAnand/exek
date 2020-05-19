9cf32f28c6492801d6e28bc306d335ceefd536b0
Sambhav Anand <sambhav182@gmail.com>
2020-05-19 17:48:44 +0530

    Process.env.prot

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const {allShortcuts} = require('./shortcuts')
 const {helpers} = require('./util')
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/getShortcuts', (req, res) => {
     res.send(allShortcuts)
