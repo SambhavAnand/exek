9e622cf158f6d3df3aa4951d61bf2fec9923b402
Sambhav Anand <sambhav182@gmail.com>
2020-05-21 22:29:28 +0530

    Added finder shortcuts

--- server/shortcuts/index.js
+++ server/shortcuts/index.js
@@ -4,14 +4,15 @@ const { macShortcuts } = require('./MacOS')
 const { notionShortcuts } = require('./Notion')
 const { calendarShortcuts } = require('./Calendar')
 const { atomShortcuts } = require('./Atom')
-
+const { finderShortcuts } = require('./Finder')
 const allShortcuts = {
     "MacOS": macShortcuts,
     "Google Chrome": chromeShortcuts,
     "Microsoft Word": wordShortcuts,
     "Notion": notionShortcuts,
     "Calendar": calendarShortcuts,
-    "Atom": atomShortcuts
+    "Atom": atomShortcuts,
+    "Finder": finderShortcuts
 }
 
 module.exports = {
