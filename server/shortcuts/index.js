a92d842562bf62d91f8323abdfa769003f371952
Sambhav Anand <sambhav182@gmail.com>
2020-05-23 14:47:12 +0530

    Added excel shortcuts

--- server/shortcuts/index.js
+++ server/shortcuts/index.js
@@ -6,11 +6,8 @@ const { calendarShortcuts } = require('./Calendar')
 const { atomShortcuts } = require('./Atom')
 const { finderShortcuts } = require('./Finder')
 const { safariShortcuts } = require('./Safari')
-<<<<<<< HEAD
 const { sublimeShortcuts } = require('./Sublime')
-=======
 const { excelShortcuts } = require('./MicrosoftExcel')
->>>>>>> 62bcb4cb57e5368826599c9b3f4c6f9afdb12ec2
 
 const allShortcuts = {
     "MacOS": macShortcuts,
@@ -21,11 +18,8 @@ const allShortcuts = {
     "Atom": atomShortcuts,
     "Finder": finderShortcuts,
     "Safari": safariShortcuts,
-<<<<<<< HEAD
-    "Sublime Text": sublimeShortcuts
-=======
+    "Sublime Text": sublimeShortcuts,
     "Microsoft Excel": excelShortcuts
->>>>>>> 62bcb4cb57e5368826599c9b3f4c6f9afdb12ec2
 }
 
 module.exports = {
